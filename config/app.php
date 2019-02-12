94a8401c1b0e5449c393d8959c571fee10e62a06
DungLQ159 <quangdunglu159@gmail.com>
2019-02-12 15:46:28 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Orangehill\Iseed\IseedServiceProvider::class,
+        Way\Generators\GeneratorsServiceProvider::class,
+        Xethron\MigrationsGenerator\MigrationsGeneratorServiceProvider::class,
 
     ],
 
